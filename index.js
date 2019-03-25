d17d97164edd8276fe6c6f0fdee86ef35e19b127
renmingming <renmingming@iyich.com>
2019-03-25 11:16:43 +0800

    no message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const http = require('http')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end(JSON.stringify({userName: 'helloWorld'}))
-}).listen(3389)
+  res.end(JSON.stringify({userName: 'helloWorldRenmingming'}))
+}).listen(8080)
 console.log('NodeJS Server running')
